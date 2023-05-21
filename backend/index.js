bd2067d85405c60eba6a51a4846fe5a2108e2084
arsame-th <shizennohanashi@gmail.com>
2023-05-21 18:26:23 +0900

    save crud api

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 const app = express();
-
+const postRourter = require("./routes/postRoutes")
 const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
 const connectWithRetry = () => {
@@ -15,9 +15,14 @@ const connectWithRetry = () => {
 }
 connectWithRetry()
 
+app.use(express.json());
 
+ //localhost:3000/api/v1/post/
 app.get("/", (req,res) => {
-    res.send("<h2>Hi There xxxx</h2>")
+    res.send("<h2>Hi There ??</h2>")
 })
+
+app.use("/api/v1/posts"  , postRourter)
+
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log("listening on port " + port))
